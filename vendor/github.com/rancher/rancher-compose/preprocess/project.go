09c5e033235a4e39bfd374b1cc7adcbd602d98d8
rajashree <rajashree.28m@gmail.com>
2016-11-07 09:51:19 -0800

    Updates vendor folder

--- vendor/github.com/rancher/rancher-compose/preprocess/project.go
+++ vendor/github.com/rancher/rancher-compose/preprocess/project.go
@@ -1,28 +1,69 @@
 package preprocess
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/docker/libcompose/config"
+	"github.com/rancher/rancher-compose/utils"
 )
 
-func PreprocessServiceMap(serviceMap config.RawServiceMap) (config.RawServiceMap, error) {
-	newServiceMap := make(config.RawServiceMap)
+type BindingProperty struct {
+	Services map[string]Service `json:"services"`
+}
 
-	for k, v := range serviceMap {
-		newServiceMap[k] = make(config.RawService)
+type Service struct {
+	Labels map[string]interface{} `json:"labels"`
+	Ports  []interface{}          `json:"ports"`
+}
 
-		for k2, v2 := range v {
-			if k2 == "environment" || k2 == "labels" {
-				newServiceMap[k][k2] = Preprocess(v2, true)
-			} else {
-				newServiceMap[k][k2] = Preprocess(v2, false)
+func PreprocessServiceMap(bindingsBytes []byte) func(serviceMap config.RawServiceMap) (config.RawServiceMap, error) {
+	return func(serviceMap config.RawServiceMap) (config.RawServiceMap, error) {
+		newServiceMap := make(config.RawServiceMap)
+
+		var binding BindingProperty
+		var bindingsServices []string
+
+		if bindingsBytes != nil {
+			err := json.Unmarshal(bindingsBytes, &binding)
+			if err != nil {
+				return nil, err
 			}
 
+			for k := range binding.Services {
+				bindingsServices = append(bindingsServices, k)
+			}
 		}
-	}
 
-	return newServiceMap, nil
+		for k, v := range serviceMap {
+			newServiceMap[k] = make(config.RawService)
+			if bindingsBytes != nil {
+				if utils.Contains(bindingsServices, k) == true {
+					labelMap := make(map[interface{}]interface{})
+					for key, value := range binding.Services[k].Labels {
+						labelMap[interface{}(key)] = value
+					}
+					if len(labelMap) != 0 {
+						v["labels"] = labelMap
+					}
+					if len(binding.Services[k].Ports) > 0 {
+						v["ports"] = binding.Services[k].Ports
+					}
+				}
+			}
+			for k2, v2 := range v {
+				if k2 == "environment" || k2 == "labels" {
+					newServiceMap[k][k2] = Preprocess(v2, true)
+				} else {
+					newServiceMap[k][k2] = Preprocess(v2, false)
+				}
+
+			}
+		}
+
+		return newServiceMap, nil
+	}
 }
 
 func Preprocess(item interface{}, replaceTypes bool) interface{} {
@@ -47,9 +88,62 @@ func Preprocess(item interface{}, replaceTypes bool) interface{} {
 		return newArray
 
 	default:
-		if replaceTypes {
+		if replaceTypes && item != nil {
 			return fmt.Sprint(item)
 		}
+		return item
+	}
+}
+
+func TryConvertStringsToInts(serviceMap config.RawServiceMap) (config.RawServiceMap, error) {
+	newServiceMap := make(config.RawServiceMap)
+
+	for k, v := range serviceMap {
+		newServiceMap[k] = make(config.RawService)
+
+		for k2, v2 := range v {
+			if k2 == "disks" || k2 == "load_balancer_config" || k2 == "health_check" || k2 == "scale_policy" || k2 == "upgrade_strategy" || k2 == "service_schemas" {
+				newServiceMap[k][k2] = tryConvertStringsToInts(v2, true)
+			} else {
+				newServiceMap[k][k2] = tryConvertStringsToInts(v2, false)
+			}
+
+		}
+	}
+
+	return newServiceMap, nil
+}
+
+func tryConvertStringsToInts(item interface{}, replaceTypes bool) interface{} {
+	switch typedDatas := item.(type) {
+
+	case map[interface{}]interface{}:
+		newMap := make(map[interface{}]interface{})
+
+		for key, value := range typedDatas {
+			newMap[key] = tryConvertStringsToInts(value, replaceTypes)
+		}
+		return newMap
+
+	case []interface{}:
+		// newArray := make([]interface{}, 0) will cause golint to complain
+		var newArray []interface{}
+		newArray = make([]interface{}, 0)
+
+		for _, value := range typedDatas {
+			newArray = append(newArray, tryConvertStringsToInts(value, replaceTypes))
+		}
+		return newArray
+
+	case string:
+		lineAsInteger, err := strconv.Atoi(typedDatas)
+
+		if replaceTypes && err == nil {
+			return lineAsInteger
+		}
+
+		return item
+	default:
 		return item
 	}
 }
